101479e82aab12f526a783085162433f08839968
thagiuda <takahiro.hagiuda@gmail.com>
2022-07-10 19:27:05 +0900

    ✨ カウンターを設置して、useStateでカウントアップできるようにした

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,21 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    alert(foo);
-    e.preventDefault();
-  }, []);
+  const [count, setCount] = useState(1);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
-    console.log("マウント");
+    // console.log("マウント");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("アンマウント");
+      // console.log("アンマウント");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -28,9 +28,8 @@ export default function Home() {
         <title>Index page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
