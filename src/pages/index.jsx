cf5a9221e5368aaabd8d532639cda56b49dd29a6
thagiuda <takahiro.hagiuda@gmail.com>
2022-07-11 10:41:50 +0900

    ✨useStateで文字列と真偽値を初期値にとったときの解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,8 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -14,8 +16,19 @@ export default function Home() {
     }
   }, [count]);
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 10) {
+      alert("aaa");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   useEffect(() => {
-    console.log("foo");
     document.body.style.backgroundColor = "lightblue";
     return () => {
       document.body.style.backgroundColor = "";
@@ -28,8 +41,10 @@ export default function Home() {
         <title>Index page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
