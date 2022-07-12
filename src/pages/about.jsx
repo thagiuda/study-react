44fd970d0d716c159f065edb4b0b213f62d8c1f6
thagiuda <takahiro.hagiuda@gmail.com>
2022-07-12 14:12:04 +0900

    🎨 custom hooksを使って上手くまとめました

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,35 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
 import styles from "src/styles/Home.module.css";
-import { Main } from "src/components/Main";
-import { Header } from "src/components/Header";
 
-export default function Home() {
+import { Header } from "src/components/Header";
+import { Main } from "src/components/Main";
+import { Footer } from "src/components/Footer";
+
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+
+export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
+
   return (
     <div className={styles.container}>
       <Head>
         <title>About page</title>
       </Head>
       <Header />
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="about" />
       <Footer />
     </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,50 +1,18 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
+
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { Main } from "src/components/Main";
+import { Footer } from "src/components/Footer";
+
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 10) {
-      alert("aaa");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
